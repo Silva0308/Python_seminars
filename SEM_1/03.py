a18b5d8f9da7b1ead2d6da1ad35262b8cf12db7d
Валерия Васильева <test@MacBook-Air-test-2.local>
2022-09-22 11:14:44 +0300

    дробь

--- SEM_1/03.py
+++ SEM_1/03.py
@@ -6,3 +6,13 @@ if n>0:
 else:
     for i in range(-n,n-1, -1):
         print(i,end=' ')
+
+# num = int(input())
+#neg_num = -num
+
+#while num != neg_num:
+   # print(neg_num, end=", ")
+    #if num > 0:
+      #  neg_num += 1
+    #else:
+      # neg_num -= 1
