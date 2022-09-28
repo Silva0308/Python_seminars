0c5d15847ab7a272bd28f2330cef9f4157d8f63a
Silva0308 <travinina_valery@mail.ru>
2022-09-28 17:12:18 +0300

    correction 4 task

--- SEM_2/HW/04.py
+++ SEM_2/HW/04.py
@@ -1,13 +1,18 @@
 # 4. Напишите программу, которая принимает на вход 2 числа.
 # Задайте список из N элементов, заполненных числами из промежутка [-N, N].
 # Найдите произведение элементов на указанных позициях(не индексах).
-from random import randint
-n = int(input("Введите количество элементов: "))
+n = int(input("Введите число, ограничивающее диапазон: "))
 pos_1 = int(input("Введите позицию первого множителя: "))
 pos_2 = int(input("Введите позицию второго множителя: "))
-array1 = [randint(-n, n) for x in range(n)]
-print(array1)
-if 0 < pos_1 <= n and 0 < pos_2 <= n:
-    print(array1[pos_1-1]*array1[pos_2-1])
+new_list=[]
+if n>0:
+    for x in range(-n,n+1):
+        new_list.append(x)
+else:
+    for x in range(-n,n-1, -1):
+        new_list.append(x)
+print(new_list)
+if 0 < pos_1 <= len(new_list) and 0 < pos_2 <= len(new_list):
+    print(new_list[pos_1-1]*new_list[pos_2-1])
 else:
     print("Таких элементов в массиве нет")
