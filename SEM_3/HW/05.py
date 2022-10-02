08f7558fadea5f3eec44efa5ed2d6f1ce4dbba3d
Silva0308 <travinina_valery@mail.ru>
2022-10-02 09:11:10 +0300

    fibonacci

--- SEM_3/HW/05.py
+++ SEM_3/HW/05.py
@@ -1,10 +1,10 @@
 # 5. ** Задайте число. Составьте список чисел Фибоначчи, в том числе для отрицательных индексов.
 num = int(input("Задайте число: "))
-fib1 = fib2 = 1
-list_num=[fib1,fib2]
-for i in range(2, num):
+list_num=[0]
+fib1 = 1
+fib2 = 0
+for i in range(num):
     fib1, fib2 = fib2, fib1 + fib2
     list_num.append(fib2)
     list_num.insert(0,((-1) **(i) * fib2))
 print(list_num)
-
