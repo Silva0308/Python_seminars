06255d11ad812041cad1646ca3e25778d96bb4ee
Silva0308 <travinina_valery@mail.ru>
2022-10-05 17:55:35 +0300

    list without copyies

--- SEM_4/HW/02.py
+++ SEM_4/HW/02.py
@@ -1,12 +1,7 @@
 # 2. Задайте натуральное число N. Напишите программу, которая составит список простых множителей числа N.
 def fact(n):
     i = 2
-    if n<0:
-        n*= -1
-        result = [-1]
-    elif n==0:
-        result = 0
-    elif n==1:
+    if n==1:
          result=1
     else:
         result=[]
@@ -20,4 +15,8 @@ def fact(n):
         result.append(n)
     return result
 
-print(fact(int(input("введите число: "))))
+N = int(input("введите натуральное число: "))
+if N<1:
+    print("Это не натуральное число")
+else:
+    print(fact(N))
---
+++ SEM_4/HW/03.py
@@ -0,0 +1,9 @@
+# 3. Задайте последовательность чисел. 
+# Напишите программу, которая выведет список неповторяющихся элементов исходной последовательности в том же порядке.
+from random import randint
+n = int(input("Введите количество чисел: "))
+my_list = [randint(0, 10) for x in range(n)]
+print (my_list)
+new_list = []
+[new_list.append(i) for i in my_list if i not in new_list]
+print(f"Список из неповторяющихся элементов: {new_list}")
