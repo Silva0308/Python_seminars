70ff81a78b7a39aa3c26e67599fd4a8313f4b024
Silva0308 <travinina_valery@mail.ru>
2022-10-11 13:53:25 +0300

    is multiple

--- SEM_6/HW/01.py
+++ SEM_6/HW/01.py
@@ -1,17 +1,22 @@
-# 1. Представлен список чисел. Необходимо вывести элементы исходного списка, 
+# 1. Представлен список чисел. Необходимо вывести элементы исходного списка,
 # значения которых больше предыдущего элемента. Use comprehension.
 import numbers
 from random import randint
-def new_list(n:int):
-    my_list=[randint(1, 101) for _ in range(n)]
+
+
+def new_list(n: int):
+    my_list = [randint(1, 101) for _ in range(n)]
     return my_list
 
-def sort_list(numbers:list):
-    sort_num=[numbers[i] for i in range(1,len(numbers)) if numbers[i]>numbers[i-1]]
+
+def sort_list(numbers: list):
+    sort_num = [numbers[i]
+                for i in range(1, len(numbers)) if numbers[i] > numbers[i-1]]
     return sort_num
+
 
 n = int(input('Put the number: '))
 task = new_list(n)
 print(task)
-sorted_task=sort_list(task)
+sorted_task = sort_list(task)
 print(sorted_task)
