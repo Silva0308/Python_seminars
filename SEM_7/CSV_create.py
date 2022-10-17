653812dde2a944a69710dc4da35ff6a09e684d96
Silva0308 <travinina_valery@mail.ru>
2022-10-17 16:42:47 +0300

    запись и создание словаря

--- SEM_7/CSV_create.py
+++ SEM_7/CSV_create.py
@@ -1,9 +1,6 @@
-import csv
-from user_interface import get_info as gi
+
 from pathlib import Path
-def creating ():
-    file = Path("Python_seminars", "SEM_7",'Phonebook.csv')
-    with open (file, 'w', encoding = 'utf-8') as data:
-        file_writer = csv.writer(data, delimiter = ",", lineterminator="\r")
-        file_writer.writerow(['Фамилия', 'Имя','Номер телефона','Описание'])
-        file_writer.writerow(gi())
+
+
+def creating():
+    file = Path("Python_seminars", "SEM_7", 'Phonebook.csv')
--- SEM_7/file_writing.py
+++ SEM_7/file_writing.py
@@ -1,16 +1,25 @@
-from user_interface import get_info as gi
+
+from get_data import last_names, get_name, telephone, info, dic
 from pathlib import Path
-import csv
 
-info = gi()
-def writing_scv ():
-    file = Path("Python_seminars", "SEM_7",'Phonebook.csv')
-    with open (file, 'w', encoding = 'utf-8') as data:
-        file_writer = csv.writer(data, delimiter = ",", lineterminator="\r")
-        file_writer.writerow(['Фамилия', 'Имя','Номер телефона','Описание'])
-        file_writer.writerow(info)
+phonebook = dic(last_names(), get_name(), telephone(), info())
 
-def writing_txt ():
-    file = Path("Python_seminars", "SEM_7",'Phonebook.txt')
-    with open (file, 'a', encoding = 'utf-8') as data:
-        data.write(f'Фамилия: {info[0]}\n\nИмя: {info[1]}\n\nНомер телефона: {info[2]}\n\nОписание: {info[3]}\n\n\n')
+
+def writing_scv(phonebook):
+    file = Path("Python_seminars", "SEM_7", 'Phonebook.csv')
+    with open(file, 'a', encoding='utf-8') as data:
+        data.write('№,Фамилия,Имя,Номер телефона,Описание\n')
+        for k, v in phonebook.items():
+            data.write(
+            f'{k},{v[0]},{v[1]},{v[2]},{v[3]}\n')
+
+def writing_txt(phonebook):
+    file = Path("Python_seminars", "SEM_7", 'Phonebook.txt')
+    with open(file, 'a', encoding='utf-8') as data:
+        for k, v in phonebook.items():
+            data.write(
+            f'№ {k}\n\nФамилия: {v[0]}\n\nИмя: {v[1]}\n\nНомер телефона: {v[2]}\n\nОписание: {v[3]}\n\n\n')
+
+
+writing_txt(phonebook)
+writing_scv(phonebook)
